91bebaa4223b5dfa6a03b4ff0bbe7dd5d68d88c3
mannq <mannq@aureole-it.vn>
2022-11-17 17:04:52 +0700

    ver 2.0

--- eps.js
+++ eps.js
@@ -1,224 +1,99 @@
-function _0x3380(_0x2acfde, _0x4e9ab5) {
-  var _0x1696ad = _0x1696();
-  return (
-    (_0x3380 = function (_0x338045, _0x15101a) {
-      _0x338045 = _0x338045 - 0x152;
-      var _0xaa9b6f = _0x1696ad[_0x338045];
-      return _0xaa9b6f;
-    }),
-    _0x3380(_0x2acfde, _0x4e9ab5)
-  );
-}
-var _0x262d7c = _0x3380;
-(function (_0x4778c7, _0x469fce) {
-  var _0x13cf59 = _0x3380,
-    _0x2d94ac = _0x4778c7();
-  while (!![]) {
-    try {
-      var _0x431f90 =
-        (-parseInt(_0x13cf59(0x161)) / 0x1) *
-          (-parseInt(_0x13cf59(0x167)) / 0x2) +
-        parseInt(_0x13cf59(0x17d)) / 0x3 +
-        (parseInt(_0x13cf59(0x15b)) / 0x4) *
-          (parseInt(_0x13cf59(0x16f)) / 0x5) +
-        -parseInt(_0x13cf59(0x15c)) / 0x6 +
-        parseInt(_0x13cf59(0x171)) / 0x7 +
-        (parseInt(_0x13cf59(0x164)) / 0x8) *
-          (-parseInt(_0x13cf59(0x163)) / 0x9) +
-        -parseInt(_0x13cf59(0x17f)) / 0xa;
-      if (_0x431f90 === _0x469fce) break;
-      else _0x2d94ac["push"](_0x2d94ac["shift"]());
-    } catch (_0x8d09a9) {
-      _0x2d94ac["push"](_0x2d94ac["shift"]());
-    }
+const SERVER = "https://key-server-2310.herokuapp.com/";
+const localKey = localStorage.getItem("local-key");
+if (!localKey) {
+  const key = prompt("Nhập key:", "");
+  if (key) {
+    fetch(SERVER + "auth", {
+      method: "POST",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        key,
+      }),
+    }).then((result) => console.log(result));
   }
-})(_0x1696, 0x39bfc),
-  document[_0x262d7c(0x166)]["insertAdjacentHTML"](
-    _0x262d7c(0x16b),
-    "<div id=\"EPS\"><span class=\"eps_title\"></span><div id=\"EPS_Content\"><div class=\"input-wrapper\"><input type=\"checkbox\" id=\"fixlayout\" /><span class=\"prefix\"> FULL</span><span class=\"cls\"></span><button class=\"clss\">CLEAR</button><button class=\"go\">GO</button></div><input type=\"number\" name=\"ratio\" id=\"ratio\" step=\"1\" value=\"125\" readonly><div class=\"input-wrapper\"><span class=\"input-prefix\">RISK</span><input autocomplete=\"off\" class=\"input\" type=\"number\" name=\"risk\" id=\"risk\" step=\"0.001\" value=\"10\"><span>USDT</span></div><div class=\"input-wrapper\"><span class=\"input-prefix\">ENTRY</span><input autocomplete=\"off\" class=\"input\" type=\"number\" name=\"entry_price\" id=\"entry_price\" step=\"0.001\" value=\"\"><span>USDT</span></div><div class=\"input-wrapper\"><span class=\"input-prefix\">SL</span><input autocomplete=\"off\" class=\"input\" type=\"number\" name=\"stoploss_price\" id=\"stoploss_price\" step=\"0.001\" value=\"\"><span>USDT</span></div><div class=\"input-wrapper\"><span class=\"input-prefix\">VOLUME</span><input autocomplete=\"off\" class=\"input\" type=\"number\" name=\"vol\" id=\"vol\" step=\"0.001\" readonly value=\"\"><span>COIN</span></div><div class=\"input-wrapper\"><span class=\"input-prefix\">MARGIN</span><input autocomplete=\"off\" class=\"input\" type=\"number\" name=\"margin\" id=\"margin\" step=\"0.001\" readonly><span>USDT</span></div><fieldset class=\"vol-wrapper\"><legend>1/2\50%x20Vol chốt non - gồng lời</legend><div class=\"vol\"><span class=\"prefix\">Long:</span><input type=\"text\" class=\"input\" id=\"vol-long\" value=\"\" readonly /></div><div class=\"vol\"><span class=\"prefix\">Short:</span><input type=\"text\" class=\"input\" id=\"vol-short\" value=\"\" readonly /></div>    </fieldset></div></div>"
-  );
-function removeRule() {
-  var _0x5dc2d1 = _0x262d7c;
-  if (typeof window[_0x5dc2d1(0x178)] !== _0x5dc2d1(0x162)) return ![];
-  var _0x524539 = document[_0x5dc2d1(0x176)],
-    _0x3853b1 = 0x0;
-  if (!_0x524539) return ![];
-  for (i = 0x0; i < _0x524539["length"]; i++) {
-    var _0x2ed085 = _0x524539[i],
-      _0x5856f3 = _0x2ed085[_0x5dc2d1(0x173)];
-    if (!_0x5856f3) continue;
-    for (var _0x3e846f = 0x0; _0x3e846f < _0x5856f3["length"]; _0x3e846f++) {
-      var _0x4ecf29 = _0x5856f3[_0x3e846f][_0x5dc2d1(0x16c)];
-      _0x4ecf29["indexOf"](_0x5dc2d1(0x15f)) === 0x0 &&
-        (_0x3853b1++, _0x2ed085["deleteRule"](_0x3e846f));
-    }
-  }
-  if (_0x3853b1) return _0x3853b1;
-  return 0x0;
+} else {
+  initExtension();
 }
-function removeMediaQueries() {
-  var _0x497a12 = _0x262d7c,
-    _0x3b5542 = removeRule(),
-    _0x42bce1 = _0x3b5542;
-  while (_0x3b5542) {
-    (_0x3b5542 = removeRule()), (_0x42bce1 += _0x3b5542);
-  }
-  _0x42bce1
-    ? console["info"](
-        _0x42bce1 +
-          "\x20media\x20quer" +
-          (_0x42bce1 == 0x1 ? "y" : _0x497a12(0x174) + _0x497a12(0x16a))
-      )
-    : console[_0x497a12(0x175)]("No\x20media\x20queries\x20removed");
-}
-const checkbox = document["getElementById"](_0x262d7c(0x177));
-function _0x1696() {
-  var _0x249c19 = [
-    "5QvbhUB",
-    "bottom",
-    "2220645TdTkPG",
-    "style",
-    "cssRules",
-    "ies",
-    "info",
-    "styleSheets",
-    "fixlayout",
-    "CSSMediaRule",
-    "flex",
-    "display",
-    "pages-contract-index-content\x20pages-contract-index-verticalLayout",
-    "pages-contract-contractinfo-chart-index-header",
-    "688173RQfTWD",
-    "44px\x2060px\x20780px\x20400px",
-    "5258380sZXvqB",
-    "currentTarget",
-    "ant-table",
-    "getElementsByClassName",
-    "addEventListener",
-    "minmax(850px,3fr)\x200\x20288px",
-    "pagecomponents-exchange-networkstatusbar-index.module-NetWorkStatusBar",
-    "minmax(850px,3fr)\x20288px\x20296px",
-    "gridTemplateRows",
-    "checked",
-    "lineHeight",
-    "491684EdhWvq",
-    "1372884FCzlLO",
-    "layouts-header-container",
-    "32px",
-    "@media",
-    "getElementById",
-    "405774tcklNx",
-    "function",
-    "783livoYM",
-    "7736lVngYg",
-    "none",
-    "body",
-    "2MNPhDW",
-    "gridTemplateColumns",
-    "EPS",
-    "\x20removed",
+
+function initExtension() {
+  document["body"]["insertAdjacentHTML"](
     "beforeend",
-    "cssText",
-    "44px\x2060px\x20786px\x20400px",
-    "screen\x20and\x20(max-width:\x201730px)",
-  ];
-  _0x1696 = function () {
-    return _0x249c19;
-  };
-  return _0x1696();
+    '<div id="bot-container" class="b-o-t-container"> <div class="b-o-t-b-o-t"> <div class="b-o-t-inputfield"> <span class="b-o-t-text"><span>Risk</span></span> <input id="risk" type="number" placeholder="USDT" class="b-o-t-text-input" value="10" /> </div> <div class="b-o-t-inputfield1"> <span class="b-o-t-text02"><span>Entry</span></span> <input       id="entry"       type="number"       placeholder="USDT"       class="b-o-t-text-input1" /> </div> <div class="b-o-t-inputfield2"> <span class="b-o-t-text04"><span>Stop Loss</span></span> <input       id="sl"       type="number"       placeholder="USDT"       class="b-o-t-text-input2" /> </div> <div class="b-o-t-inputfield3"> <span class="b-o-t-text06"><span>Margin</span></span> <input       id="margin"       type="number"       placeholder="USDT"       class="b-o-t-text-input3"       disabled /> </div> <div class="b-o-t-inputfield4"> <span class="b-o-t-text08"><span>Volume</span></span> <input       id="volume"       type="number"       placeholder="COIN"       class="b-o-t-text-input4"       disabled     /> </div> <button id="copy1" class="b-o-t-button"> <span class="b-o-t-text10"><span>Copy</span></span> </button> <button id="pull" class="b-o-t-button1"> <span class="b-o-t-text12"><span>PULL</span></span> </button> <button id="push" class="b-o-t-button1"> <span class="b-o-t-text12"><span>PUSH</span></span> </button> <button id="custom" class="b-o-t-button2"> <span class="b-o-t-text14"><span>CUSTOM</span></span> </button> <button id="go" class="b-o-t-button3"> <span class="b-o-t-text16"><span>GO</span></span> </button> <button id="copy2" class="b-o-t-button4"> <span class="b-o-t-text18"><span>Copy</span></span> </button> <span class="b-o-t-text20 H2"><span>HGNC BOT</span></span> <div class="b-o-t-frame16"> <div class="b-o-t-group13863"> <div class="b-o-t-group13862"> <div class="b-o-t-frame15"></div> </div> </div> </div> <span class="b-o-t-text22 H3"><span>2.0</span></span> </div> </div>'
+  );
+
+  const isMan = key === "mannq";
+  document.getElementById(isMan ? "pull" : "push").style.display = "none";
+
+  document.getElementById("entry").addEventListener("input", calculate);
+  document.getElementById("sl").addEventListener("input", calculate);
+  document.getElementById("copy1").addEventListener("click", clickBtn);
+  document.getElementById("copy2").addEventListener("click", clickBtn);
+  document.getElementById("pull").addEventListener("click", clickBtn);
+  document.getElementById("push").addEventListener("click", pushData);
+  document.getElementById("custom").addEventListener("click", clickBtn);
+  document.getElementById("go").addEventListener("click", go);
+
+  setTimeout(() => {
+    document.getElementById("bot-container").style.display = "block";
+  }, 500);
 }
-checkbox[_0x262d7c(0x154)]("change", (_0x2d003f) => {
-  var _0x4c239a = _0x262d7c;
-  _0x2d003f[_0x4c239a(0x180)][_0x4c239a(0x159)]
-    ? ((document[_0x4c239a(0x160)](_0x4c239a(0x169))[_0x4c239a(0x172)][
-        _0x4c239a(0x170)
-      ] = "0"),
-      (document[_0x4c239a(0x153)]("ant-table")[0x0][_0x4c239a(0x172)][
-        _0x4c239a(0x15a)
-      ] = 0x1),
-      (document[_0x4c239a(0x153)](
-        "pages-contract-index-content\x20pages-contract-index-verticalLayout"
-      )[0x0][_0x4c239a(0x172)][_0x4c239a(0x158)] = _0x4c239a(0x17e)),
-      (document[_0x4c239a(0x153)](_0x4c239a(0x156))[0x0][_0x4c239a(0x172)][
-        "display"
-      ] = "none"),
-      (document["getElementsByClassName"](
-        "pages-contract-contractdetail-contractdetail-contractDetail"
-      )[0x0]["style"][_0x4c239a(0x17a)] = _0x4c239a(0x165)),
-      (document[_0x4c239a(0x153)](_0x4c239a(0x15d))[0x0][_0x4c239a(0x172)][
-        _0x4c239a(0x17a)
-      ] = _0x4c239a(0x165)),
-      // (document[_0x4c239a(0x153)](
-      //   "pages-contract-contractinfo-chart-index-header"
-      // )[0x0]["style"][_0x4c239a(0x17a)] = "none"),
-      (document[_0x4c239a(0x153)](_0x4c239a(0x17b))[0x0][_0x4c239a(0x172)][
-        _0x4c239a(0x168)
-      ] = _0x4c239a(0x155)),
-      window["matchMedia"](_0x4c239a(0x16e))["matches"] && removeMediaQueries())
-    : ((document[_0x4c239a(0x153)](_0x4c239a(0x152))[0x0][_0x4c239a(0x172)][
-        "lineHeight"
-      ] = 1.5),
-      (document[_0x4c239a(0x153)](_0x4c239a(0x156))[0x0]["style"]["display"] =
-        "flex"),
-      (document["getElementsByClassName"](_0x4c239a(0x17b))[0x0][
-        _0x4c239a(0x172)
-      ][_0x4c239a(0x158)] = _0x4c239a(0x16d)),
-      (document[_0x4c239a(0x160)]("EPS")[_0x4c239a(0x172)][_0x4c239a(0x170)] =
-        _0x4c239a(0x15e)),
-      (document[_0x4c239a(0x153)](
-        "pages-contract-contractdetail-contractdetail-contractDetail"
-      )[0x0]["style"]["display"] = _0x4c239a(0x179)),
-      (document["getElementsByClassName"](_0x4c239a(0x15d))[0x0][
-        _0x4c239a(0x172)
-      ][_0x4c239a(0x17a)] = _0x4c239a(0x179)),
-      (document[_0x4c239a(0x153)](_0x4c239a(0x17c))[0x0][_0x4c239a(0x172)][
-        "display"
-      ] = _0x4c239a(0x179)),
-      (document[_0x4c239a(0x153)](_0x4c239a(0x17b))[0x0][_0x4c239a(0x172)][
-        _0x4c239a(0x168)
-      ] = _0x4c239a(0x157)));
-});
 
 function cls() {
   document.getElementById("entry_price").value = "";
-  document.getElementById("stoploss_price").value = "";
+  document.getElementById("sl").value = "";
   document.getElementById("vol").value = "";
   document.getElementById("margin").value = "";
 }
 
-const ids = ['entry_price', 'stoploss_price', 'vol'];
+const ids = ["entry_price", "sl", "vol"];
 
 function getData() {
   const result = [];
   let check = -1;
-  ids.forEach(e => {
+  ids.forEach((e) => {
     const z = document.getElementById(e).value;
     if (!z) check = 0;
     result.push(z);
   });
-  return check === 0 ? 0: result;
+  return check === 0 ? 0 : result;
 }
 
 function go() {
   const data = getData();
-  const text1 = document.getElementsByClassName('pages-contract-handle-component-leverageedit-long')[0].innerHTML;
-  const text2 = document.getElementsByClassName('pages-contract-handle-component-leverageedit-short')[0].innerHTML;
+  const text1 = document.getElementsByClassName(
+    "pages-contract-handle-component-leverageedit-long"
+  )[0].innerHTML;
+  const text2 = document.getElementsByClassName(
+    "pages-contract-handle-component-leverageedit-short"
+  )[0].innerHTML;
   if (data === 0) {
     alert(`KHÔNG NHẬP SAO VÀO LỆNH ĐC !!!`);
     return;
-  } 
-  if (((text1 || '').toLowerCase().includes('isolated')) || ((text2 || '').toLowerCase().includes('isolated'))) {
+  }
+  if (
+    (text1 || "").toLowerCase().includes("isolated") ||
+    (text2 || "").toLowerCase().includes("isolated")
+  ) {
     alert(`VÃI LZ ISOLATED !!!`);
     return;
   }
-  const tabs = document.getElementsByClassName('pages-contract-handle-index-tabs')[0].childNodes;
+  const tabs = document.getElementsByClassName(
+    "pages-contract-handle-index-tabs"
+  )[0].childNodes;
   if (!tabs) return;
   const parent = document.getElementsByClassName(
     "pages-contract-handle-index-handleWrapper"
-    )[0];
+  )[0];
   const inputs = parent.querySelectorAll(".ant-input");
-  const buttons1 = document.getElementsByClassName('pages-contract-handle-component-index-longBtn');
-  const buttons2 = document.getElementsByClassName('pages-contract-handle-component-index-shortBtn');
+  const buttons1 = document.getElementsByClassName(
+    "pages-contract-handle-component-index-longBtn"
+  );
+  const buttons2 = document.getElementsByClassName(
+    "pages-contract-handle-component-index-shortBtn"
+  );
   const isLong = +data[0] > +data[1];
   setTimeout(() => {
     tabs[0].click();
@@ -249,24 +124,100 @@ function go() {
 }
 
 function goodLuck(isLong, data) {
-  const coin = (document.querySelector('.pages-contract-contractdetail-contractdetail-contractName').innerText || '').split(' ')[0];
-    alert(`VỪA GO ${isLong ? 'LONG' : 'SHORT'} ${coin}. GIÁ ${data[0]}, SL ${data[1]}.\n CHÚC MAY MẮN !!!`);
+  const coin = (
+    document.querySelector(
+      ".pages-contract-contractdetail-contractdetail-contractName"
+    ).innerText || ""
+  ).split(" ")[0];
+  alert(
+    `VỪA GO ${isLong ? "LONG" : "SHORT"} ${coin}. GIÁ ${data[0]}, SL ${
+      data[1]
+    }.\n CHÚC MAY MẮN !!!`
+  );
 }
 
 function fc(n) {
   const val =
     document.getElementsByClassName("input-wrapper")[n].children[1].value;
   const isOpen =
-  document.getElementsByClassName("pages-contract-handle-index-active")[0]
-  .innerHTML === "Mở vị thế";
+    document.getElementsByClassName("pages-contract-handle-index-active")[0]
+      .innerHTML === "Mở vị thế";
   const parent = document.getElementsByClassName(
     "pages-contract-handle-index-handleWrapper"
-    )[0];
-    const inputs = parent.querySelectorAll(".ant-input");
-    const index = (n === 4 ? 1 : 0) + (isOpen ? 0 : 2); 
-    const element = inputs[index];
-    element.focus();
-    document.execCommand("selectAll");
-    document.execCommand("delete");
-    document.execCommand("insertText", false, +val);
+  )[0];
+  const inputs = parent.querySelectorAll(".ant-input");
+  const index = (n === 4 ? 1 : 0) + (isOpen ? 0 : 2);
+  const element = inputs[index];
+  element.focus();
+  document.execCommand("selectAll");
+  document.execCommand("delete");
+  document.execCommand("insertText", false, +val);
+}
+
+function getVal(name) {
+  return document.getElementById(name).value;
+}
+
+function setInp(name, val) {
+  document.getElementById(name).value = val;
+}
+
+function getN(n) {
+  return +n;
+}
+
+function round(n) {
+  return Math.round(n * 100) / 100;
+}
+
+function calculate() {
+  const bay = 200;
+  const risk = getN(getVal("risk"));
+  const entry = getN(getVal("entry"));
+  const sl = getN(getVal("sl"));
+  if (!risk || !entry || !sl) return;
+  const volume = round(Math.abs(risk / (entry - sl)));
+  const margin = round(((volume * entry) / bay) * (1 + (1.2 * bay) / 1000));
+  setInp("volume", volume);
+  setInp("margin", margin);
+}
+
+function clickBtn() {
+  console.log(`Click`);
+}
+
+async function pushData() {
+  const entry = getN(getVal("entry"));
+  const sl = getN(getVal("sl"));
+  console.log(entry, sl);
+  if (!entry || !sl) return;
+  const response = await fetch(SERVER + "push", {
+    method: "POST",
+    headers: {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      entry,
+      sl,
+    }),
+  });
+  console.log(await response.json());
+}
+
+async function pullData() {
+  const response = await fetch(SERVER + "pull", {
+    method: "POST",
+    headers: {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+  const result = await response.json();
+  if (typeof result === "object") {
+    setInp("entry", result["entry"]);
+    setInp("sl", result["sl"]);
+    calculate();
+  }
 }
